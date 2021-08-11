b92515652039d87a78ebad394194b56b30bbf8ee
harrisonh1991 <kye0038883@gmail.com>
2021-08-11 14:56:44 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ITPopupMenu from './components/ITPopup.jsx';
-import './App.css';
+import './index.css';
 
 ReactDOM.render(
     <React.StrictMode>
